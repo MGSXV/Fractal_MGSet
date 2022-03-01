1ea1fc85507dba764f88420d16c004f7c71ef67a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-01 21:27:37 +0100

    Header file for the project

---
+++ fractol.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 21:19:37 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/03/01 21:23:14 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+#define FRACTOL_H
+
+#include<math.h>
+#include<stdlib.h>
+#include "mlx.h"
+#include "./ft_printf/ft_printf.h"
+
+#endif
