7e81725245ea70248335b1c5f84bbd65fc706dca
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-04 22:58:38 +0100

    Editing printf

--- includes/ft_printf/ft_printf.h
+++ includes/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 17:32:17 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/15 20:22:43 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/04 22:57:13 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	ft_putstr(char *s, int *count);
 void	ft_putnbr_base(char *base, long num, int *count);
 void	ft_putnbr_base_p(char *base, unsigned long num, int *count);
 char	*ft_strchr(const char *s, int c);
-int		ft_strlen(char *s);
+int		ft_strlen2(char *s);
 
 #endif
--- includes/ft_printf/ft_printf_utils.c
+++ includes/ft_printf/ft_printf_utils.c
@@ -6,13 +6,13 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:25:42 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/15 16:54:51 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/04 22:57:31 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_strlen(char *s)
+int	ft_strlen2(char *s)
 {
 	int	i;
 
