2a6444f10963d8280919fb2e26e6e8c90504fce2
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-04 22:59:19 +0100

    The new header file

--- includes/headers/fractol.h
+++ includes/headers/fractol.h
@@ -6,11 +6,19 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 16:16:24 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/04 16:16:55 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/04 22:19:59 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 #define FRACTOL_H
+
+#include "../libft/libft.h"
+#include "../ft_printf/ft_printf.h"
+#include "types.h"
+#include "mlx.h"
+#include<math.h>
+
+void	ft_error_exit(char *err_msg);
 
 #endif
