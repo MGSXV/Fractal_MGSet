1b1772356698b34e06731c7ef123de31d274a03b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-05 04:28:26 +0100

    Now returns -1 if length of strings is not equal

--- includes/libft/ft_strncmp.c
+++ includes/libft/ft_strncmp.c
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sel-kham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 11:13:48 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/11/06 11:44:06 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/05 04:25:42 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
@@ -20,6 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	str1 = (unsigned char *) s1;
 	str2 = (unsigned char *) s2;
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (-1);
 	while ((str1[i] || str2[i]) && i < n)
 	{
 		if (str1[i] - str2[i])
