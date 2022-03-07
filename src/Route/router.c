9c24fa3ff37a6e197be681942c043ec40fb7d2cf
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-07 20:08:10 +0100

    Routing initial handling

--- src/Route/router.c
+++ src/Route/router.c
@@ -6,7 +6,21 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:10:34 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/04 22:10:35 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/05 04:26:33 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../includes/headers/fractol.h"
+
+void	ft_router(char *flag)
+{
+	if (!ft_strncmp(flag, "-m", ft_strlen(flag)))
+		ft_printf("Mandelbort set");
+	else if (!ft_strncmp(flag, "-j", ft_strlen(flag)))
+		ft_printf("Julia set");
+	else if (!ft_strncmp(flag, "-s", ft_strlen(flag)))
+		ft_printf("Something else");
+	else
+		ft_error_exit("Error: Invalid argument. \
+Try \"--help\" for more information.\n");
+}
