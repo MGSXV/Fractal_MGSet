380b9ef3d416315d584a2e6d4a808f9d0386203b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-07 20:07:27 +0100

    More header files

--- includes/headers/macos_keys.h
+++ includes/headers/macos_keys.h
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   macos_keys.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/04 16:16:24 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/05 05:13:57 by sel-kham         ###   ########.fr       */
+/*   Created: 2022/03/05 05:12:51 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/03/05 06:10:01 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
+#ifndef MACOS_KEYS_H
+# define MACOS_KEYS_H
 
-# include "../libft/libft.h"
-# include "../ft_printf/ft_printf.h"
-# include "types.h"
-# include "macos_keys.h"
-# include "mlx.h"
-# include<math.h>
-
-void	ft_error_exit(char *err_msg);
-void	ft_router(char *flag);
+# define ESC_KEY 53
+# define LEFT_ARROW_KEY 123
+# define RIGHT_ARROW_KEY 124
+# define DOWN_ARROW_KEY 125
+# define UP_ARROW_KEY 126
+# define R_KEY 15
+# define W_KEY 13
+# define A_KEY 0
+# define S_KEY 1
+# define D_KEY 2
 
 #endif
--- includes/headers/types.h
+++ includes/headers/types.h
@@ -6,7 +6,20 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:53:14 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/04 18:53:15 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/05 05:04:12 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef TYPES_H
+# define TYPES_H
+
+# define WINDOW_WIDTH 1280
+# define WINDOW_HEIGHT 720
+
+typedef struct s_coor
+{
+	int	x;
+	int	y;
+}	t_coor;
+
+#endif
