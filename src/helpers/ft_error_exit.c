c8c814c77a597fa3d98ad5965aacd6b593bc71d3
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-08 23:34:45 +0100

    Editing error file

--- src/helpers/ft_error_exit.c
+++ src/helpers/ft_error_exit.c
@@ -6,20 +6,20 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:38:24 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/08 22:48:09 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/08 23:34:33 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/fractol.h"
 
-void	ft_error_exit(char *err_msg, t_data *data)
+void	ft_error_exit(char *err_msg, t_img_buff *data)
 {
 	ft_printf("%s", err_msg);
-	if (data->win_p)
-		mlx_destroy_window(data->mlx_p, data->win_p);
-	if (data->mlx_p)
-		free(data->mlx_p);
-	if (data->img_p)
-		free(data->img_p);
+	if (data->data_buff->win_p)
+		mlx_destroy_window(data->data_buff->mlx_p, data->data_buff->win_p);
+	if (data->data_buff->mlx_p)
+		free(data->data_buff->mlx_p);
+	if (data->data_buff->img_p)
+		free(data->data_buff->img_p);
 	exit(EXIT_FAILURE);
 }
