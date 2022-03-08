32a12e6c5c9e19f91dce92a2290604c3543861f1
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-08 08:30:42 +0100

    Fixing some bugs

--- src/helpers/ft_error_exit.c
+++ src/helpers/ft_error_exit.c
@@ -6,14 +6,18 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:38:24 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/05 04:13:37 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/08 08:27:38 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/fractol.h"
 
-void	ft_error_exit(char *err_msg)
+void	ft_error_exit(char *err_msg, t_data *data)
 {
 	ft_printf("%s", err_msg);
+	if (data->win_p)
+		mlx_destroy_window(data->mlx_p, data->win_p);
+	if (data->mlx_p)
+		free(data->mlx_p);
 	exit(EXIT_FAILURE);
 }
