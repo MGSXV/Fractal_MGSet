a94209101820d1a04b1c16c20ba3b648fd1f5296
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-08 08:29:17 +0100

    Add several functions and structs

--- includes/headers/fractol.h
+++ includes/headers/fractol.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 16:16:24 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/05 05:13:57 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/08 04:44:23 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,13 @@
 # include "mlx.h"
 # include<math.h>
 
-void	ft_error_exit(char *err_msg);
-void	ft_router(char *flag);
+void	ft_error_exit(char *err_msg, t_data *data);
+void	ft_free(t_data *data);
+void	ft_router(char *flag, t_data *data);
+void	ft_init_data(t_data *data);
+void	ft_new_window(t_data *data);
+void	ft_mandelbrot(t_data *data);
+int 	ft_events_router(int keynum, t_data *data);
+int		ft_close_window(t_data *data);
 
 #endif
