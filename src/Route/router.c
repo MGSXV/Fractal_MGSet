8fb3a2ea92f9a05fa04bd3b7fc1597d22bae7e48
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-08 08:31:21 +0100

    Routing functions and events

--- src/Route/router.c
+++ src/Route/router.c
@@ -6,21 +6,28 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:10:34 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/05 04:26:33 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/08 08:26:35 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/fractol.h"
 
-void	ft_router(char *flag)
+int	ft_events_router(int keynum, t_data *data)
+{
+	if (keynum == ESC_KEY)
+		ft_close_window(data);
+	return (0);
+}
+
+void	ft_router(char *flag, t_data *data)
 {
 	if (!ft_strncmp(flag, "-m", ft_strlen(flag)))
-		ft_printf("Mandelbort set");
+		ft_mandelbrot(data);
 	else if (!ft_strncmp(flag, "-j", ft_strlen(flag)))
 		ft_printf("Julia set");
 	else if (!ft_strncmp(flag, "-s", ft_strlen(flag)))
 		ft_printf("Something else");
 	else
 		ft_error_exit("Error: Invalid argument. \
-Try \"--help\" for more information.\n");
+Try \"--help\" for more information.\n", data);
 }
