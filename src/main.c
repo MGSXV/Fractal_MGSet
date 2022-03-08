279151389fb572b80ff475e5e0ba460c1adf4624
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-08 08:31:40 +0100

    Declare data buffer

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:28:53 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/05 04:22:53 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/08 06:10:09 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 int	main(int c, char **v)
 {
 	char	*flag;
+	t_data	data;
 
+	data.mlx_p = NULL;
 	flag = "--help";
 	if (c != 2)
 		ft_error_exit("Error: No valid argument was provided.\
-Try \"--help\" for more information.\n");
+Try \"--help\" for more information.\n", &data);
 	if (!ft_strncmp(v[1], flag, ft_strlen(flag)))
 	{
 		ft_printf("Here are the available options:\
@@ -28,6 +30,8 @@ Try \"--help\" for more information.\n");
 		exit(EXIT_SUCCESS);
 	}
 	else
-		ft_router(v[1]);
+		ft_router(v[1], &data);
+	// mlx_hook(data.mlx_p, ON_KEYDOWN, 0, ft_events_router, &data);
+	// mlx_loop(data.mlx_p);
 	return (0);
 }
