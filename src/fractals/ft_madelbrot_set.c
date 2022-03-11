d49ce119b51fe87bd4211157bf9f7871d788b338
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-11 20:25:32 +0100

    Preparing recurtion

--- src/fractals/ft_madelbrot_set.c
+++ src/fractals/ft_madelbrot_set.c
@@ -6,20 +6,35 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:26:20 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/11 15:46:04 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/11 20:23:05 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/fractol.h"
 
+int	draw_mandelbrot(void)
+{
+	static int	iterations;
+	t_complex	old_z;
+	t_complex	new_z;
+	t_complex	con;
+
+	if (iterations == 0)
+	{
+		old_z = complex_init(0, 0);
+		new_z = complex_init(0, 0);
+	}
+	return (0);
+}
+
 void	ft_mandelbrot(t_img_buff *data)
 {
-	int	i;
+	t_coor		coor;
+	int			max_iterations;
 
-	i = 100;
-	ft_new_window(data);
-	while (--i)
-		mg_pixel_put(data, WINDOW_WIDTH / 2 - i, WINDOW_HEIGHT / 2, 0x00FF0000);
-	mlx_put_image_to_window(data->data_buff.mlx_p, data->data_buff.win_p, data->data_buff.img_p, 0, 0);
+	coor.y = 0;
+	mg_pixel_put(data, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0x00FF0000);
+	mlx_put_image_to_window(data->data_buff.mlx_p, data->data_buff.win_p,
+		data->data_buff.img_p, 0, 0);
 	mlx_loop(data->data_buff.mlx_p);
 }
