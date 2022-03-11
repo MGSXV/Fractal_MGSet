3c6063c2e41df63f5c3584d5c77f29077e1e104d
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-11 15:48:07 +0100

    Move loop to other place

--- src/init/window_init.c
+++ src/init/window_init.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 20:12:02 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/09 23:36:01 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/10 22:38:34 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,4 @@ void	ft_new_window(t_img_buff *data)
 	data->addr = mlx_get_data_addr(data->data_buff.img_p, &data->bpp, &data->line_len, &data->endian);
 	mlx_hook(data->data_buff.win_p, ON_KEYDOWN, 0, ft_events_router, data);
 	mlx_hook(data->data_buff.win_p, DESTROY_NOTIFY, 0, ft_close_window, data);
-	mlx_loop(data->data_buff.mlx_p);
 }
