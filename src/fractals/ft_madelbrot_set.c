ddfee035148f1db10069f303f49a2f9ab67aafcd
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-11 17:05:49 +0100

    Drawing pixels using my defined pixel put

--- src/fractals/ft_madelbrot_set.c
+++ src/fractals/ft_madelbrot_set.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:26:20 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/09 02:18:11 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/11 15:46:04 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	ft_mandelbrot(t_img_buff *data)
 {
+	int	i;
+
+	i = 100;
 	ft_new_window(data);
+	while (--i)
+		mg_pixel_put(data, WINDOW_WIDTH / 2 - i, WINDOW_HEIGHT / 2, 0x00FF0000);
+	mlx_put_image_to_window(data->data_buff.mlx_p, data->data_buff.win_p, data->data_buff.img_p, 0, 0);
+	mlx_loop(data->data_buff.mlx_p);
 }
