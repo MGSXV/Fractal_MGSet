acfa85c68349248d08e7820da1c4f9f6745a5898
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-14 04:35:56 +0100

    Drawing mandelbrot set -needs to working on-

--- src/fractals/ft_madelbrot_set.c
+++ src/fractals/ft_madelbrot_set.c
@@ -6,34 +6,44 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 22:26:20 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/11 20:23:05 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/14 04:33:14 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/headers/fractol.h"
 
-int	draw_mandelbrot(void)
-{
-	static int	iterations;
-	t_complex	old_z;
-	t_complex	new_z;
-	t_complex	con;
-
-	if (iterations == 0)
-	{
-		old_z = complex_init(0, 0);
-		new_z = complex_init(0, 0);
-	}
-	return (0);
-}
-
 void	ft_mandelbrot(t_img_buff *data)
 {
-	t_coor		coor;
-	int			max_iterations;
+	t_coor		coord;
+	t_complex	new;
+	t_complex	old;
+	int			i;
 
-	coor.y = 0;
-	mg_pixel_put(data, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0x00FF0000);
+	double zoom = 1, moveX = -0.5, moveY = 0; // Edit later
+	coord.y = 0;
+	ft_new_window(data);
+	while (coord.y < WINDOW_HEIGHT)
+	{
+		coord.x = 0;
+		while (coord.x < WINDOW_WIDTH)
+		{
+			new = complex_init(0, 0);
+			old.r_part =  1.5 * (coord.x - WINDOW_WIDTH / 2) / (0.5 * zoom * WINDOW_WIDTH) + moveX;
+			old.i_part =  (coord.y - WINDOW_HEIGHT / 2) / (0.5 * zoom * WINDOW_HEIGHT) + moveY;
+			i = 0;
+			while (i < MAX_ITERATIONS)
+			{
+				new = complex_addition(complex_square(new), old);
+				if (((new.r_part * new.r_part) + (new.i_part * new.i_part)) > 4)
+					break;
+				i++;
+			}
+			if (i < MAX_ITERATIONS)
+				mg_pixel_put(data, coord.x, coord.y, 0x00FFFFFF);
+			coord.x++;
+		}
+		coord.y++;
+	}
 	mlx_put_image_to_window(data->data_buff.mlx_p, data->data_buff.win_p,
 		data->data_buff.img_p, 0, 0);
 	mlx_loop(data->data_buff.mlx_p);
