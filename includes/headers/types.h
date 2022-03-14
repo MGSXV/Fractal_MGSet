6d1c0ed67c3ebb2c2fbd48656e97bb7c3f9b62d4
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-14 04:34:50 +0100

    MAX_ITERATIONS macro

--- includes/headers/types.h
+++ includes/headers/types.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:53:14 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/13 22:56:17 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/14 04:34:03 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "mlx.h"
 # define WINDOW_WIDTH 1280
 # define WINDOW_HEIGHT 720
+# define MAX_ITERATIONS 300
 
 typedef struct s_coor
 {
