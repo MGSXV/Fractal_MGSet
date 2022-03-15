11826f4b299c911754d2c7b0c7e77323d0357063
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-15 17:31:42 +0100

    Adding new types and editing old ones

--- includes/headers/types.h
+++ includes/headers/types.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 18:53:14 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/14 23:18:38 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/15 17:30:43 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,6 @@
 # define TYPES_H
 
 # include "mlx.h"
-# define WINDOW_WIDTH 1280
-# define WINDOW_HEIGHT 720
-# define MAX_ITERATIONS 300
 
 typedef struct s_coor
 {
@@ -30,21 +27,16 @@ typedef struct s_data
 	void	*mlx_p;
 	void	*img_p;
 }	t_data;
-// bpp refers to bits per pixel
-typedef struct __image_buffer__
-{
-	t_data	data_buff;
-	char	*addr;
-	int		bpp;
-	int		line_len;
-	int		endian;
-}	t_img_buff;
 
-typedef struct s_complex
+typedef struct s_screen
 {
-	double	r_part;
-	double	i_part;
-}	t_complex;
+	int		window_width;
+	int		window_height;
+	int		max_iterations;
+	double	zoom;
+	double	move_x;
+	double	move_y;
+}	t_screen;
 
 typedef struct s_color
 {
@@ -52,12 +44,22 @@ typedef struct s_color
 	unsigned char	g;
 	unsigned char	b;
 }	t_color;
-
-typedef struct s_zoom
+// bpp refers to bits per pixel
+typedef struct __image_buffer__
 {
-	double	zoom;
-	double	move_x;
-	double	move_y;
-}	t_zoom;
+	t_data		data_buff;
+	t_screen	screen;
+	t_color		color;
+	char		*addr;
+	int			bpp;
+	int			line_len;
+	int			endian;
+}	t_img_buff;
+
+typedef struct s_complex
+{
+	double	r_part;
+	double	i_part;
+}	t_complex;
 
 #endif
