58a7998148ab462d2787a61b103be1899d1bef71
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-03-25 23:00:47 +0100

    Zoom functions

--- src/handlers/mouse_events.c
+++ src/handlers/mouse_events.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 01:17:21 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/03/20 17:12:50 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/03/21 23:24:15 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	ft_zoomin(t_coor coord, t_img_buff *data)
 {
-	
-		
+	if (data)
+		ft_printf("Zoom in : %d - %d\n", coord.x, coord.y);
 	return (0);
 }
 
 int	ft_zoomout(t_coor coord, t_img_buff *data)
 {
-	
-		
+	if (data)
+		ft_printf("Zoom out : %d - %d\n", coord.x, coord.y);
 	return (0);
 }
