b8e8112a752c48dce10db20095191b340a0f3181
Soufiane Elkhamlichi <05soufiane@gmail.com>
2022-04-17 01:11:19 +0000

    Routing burningship

--- src/Route/router.c
+++ src/Route/router.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:10:34 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/04/16 23:16:07 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/04/17 00:05:48 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ void	ft_router(char *flag, t_fractol *data)
 		data->value1 = 1;
 		ft_fractol(data, &ft_julia);
 	}
-	else if (!ft_strncmp(flag, "-s", ft_strlen(flag)))
-		ft_printf("Something else");
+	else if (!ft_strncmp(flag, "-b", ft_strlen(flag)))
+	{
+		data->value1 = 2;
+		ft_fractol(data, &ft_burning_ship);
+	}
 	else
 		ft_error_exit("Error: Invalid argument. \
 Try \"--help\" for more information.\n", data, true);
